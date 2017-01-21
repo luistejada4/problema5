745c41c1269053748eb8c16ebbd1778756fe3680
Luis Tejada <Luis Tejada@LUISTEJADA-PC>
2017-01-21 00:52:25 -0400

    Luis Tejada

---
+++ problema5/problema5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String palabra1, palabra2;
+            Console.WriteLine("Escriba una palabra: ");
+            palabra1 = Console.ReadLine();
+            Console.WriteLine("Escriba otra palabra");
+            palabra2 = Console.ReadLine();
+
+            if(palabra1[0] < palabra2[0])
+                Console.WriteLine(palabra1);
+            else
+                Console.WriteLine(palabra2);
+
+            Console.ReadKey();
+        }
+    }
+}
